e9fe5009ae3be226c7c7593a602843a0328ac870
kavindu32 <kavindumalshan.official@gmail.com>
2024-09-16 19:43:03 +0530

    this is for u Rashii

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+
+    {
+        name: "Tum Hi Ho",
+        artist: "Arijit Singh",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Siththaravi",
+        artist: "Chira Boy",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Mama Pethuwe",
+        artist: "Lasith Malinga & Minura Halwathura",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Nuba Deka Ma",
+        artist: "Sasindu Raveen",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Neth Manema",
+        artist: "Dilu Beatz",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "What Makes You Beautiful",
+        artist: "One Direction",
+        img: "music-6",
+        src: "music-6"
+    },
+    
+    ];
